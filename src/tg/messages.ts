a0e53992687d4c671499950f2024f9fc12740f2e
MikhailVrn <mihail.bindyukov@gmail.com>
2023-03-11 17:27:28 +0200

    fix: types

--- src/tg/messages.ts
+++ src/tg/messages.ts
@@ -1,4 +1,5 @@
 import { getValueSSM } from '../utils'
+import fetch from 'node-fetch'
 
 // @ts-ignore
 export const sendTelegramCommand = async (url: string, params) => {
@@ -12,11 +13,14 @@ export const sendTelegramCommand = async (url: string, params) => {
     body: JSON.stringify(params),
   })
   if (!res.ok) {
-    throw new Error(res)
+    throw new Error('Error')
   }
   const result = await res.json()
+  // @ts-ignore
   if (!result.ok) {
+    // @ts-ignore
     throw new Error(result.description)
   }
+  // @ts-ignore
   return result.result
 }
